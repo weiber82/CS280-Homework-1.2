6b853dd64939b5d0266144db5ef6366cce61a6cf
weiber82 <weiber82@gmail.com>
2017-03-06 00:26:47 +0800

    初始化專案

--- CS280-Homework-1.2/Program.cs
+++ CS280-Homework-1.2/Program.cs
@@ -6,10 +6,16 @@ using System.Threading.Tasks;
 
 namespace CS280_Homework_1._2
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            Pokemon pokemon = new Pokemon();
+            pokemon.Height = 2.0f;
+            pokemon.Weight = 12.7f;
+            pokemon.Category = "Lizard";
+            pokemon.Abilities = "M";
+            Console.WriteLine("身高{0}公尺,體重{1}磅", pokemon.Height, pokemon.Weight);
         }
     }
 }
