963a62856c9dcd0fcde0e2077515bd34f9f4e0a1
weiber82 <weiber82@gmail.com>
2017-03-05 23:55:43 +0800

    初始化專案

---
+++ CS280-Homework-1.2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS280_Homework_1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
